54669656fac0df1930461cd0b15ab6602c5fe307
Miftahus Surur Sutowo <62152669+miftahozuki@users.noreply.github.com>
2025-01-11 12:10:01 +0700

    add button expor laporan

--- app/Views/admin/laporan/js/_js_barang.php
+++ app/Views/admin/laporan/js/_js_barang.php
@@ -1,7 +1,10 @@
-<!-- SELECT2 -->
-<link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
-<link href="https://cdnjs.cloudflare.com/ajax/libs/select2-bootstrap-theme/0.1.0-beta.10/select2-bootstrap.min.css" rel="stylesheet" />
-<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+<!-- Button Export Datatables -->
+<script src="https://cdn.datatables.net/buttons/2.4.2/js/dataTables.buttons.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.4.2/js/buttons.bootstrap5.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.4.2/js/buttons.html5.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
 
 
 <script>
@@ -16,7 +19,13 @@
       $('#table_list').DataTable({
             "scrollX": true,
             "dom": 'lBfrtip',
-            "buttons": [],
+            "buttons": [
+                  {
+                        extend: 'pdf',
+                  }
+                  , 
+                  'excel'
+            ],
             "lengthMenu": [
                   [ 10, 25, 50, -1 ],
                   [ '10 rows', '25 rows', '50 rows', 'Show all' ]
