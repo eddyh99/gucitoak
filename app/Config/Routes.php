0902a2fc8c1b7adf2da954588952631ada0f1657
Miftahus Surur Sutowo <62152669+miftahozuki@users.noreply.github.com>
2025-01-12 12:30:38 +0700

    Update Routes.php

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,4 +6,4 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Auth::index');
-   
+$routes->get('/pembayaran/pelanggan/tambah', 'Pembayaran::pelanggan_tambah');
