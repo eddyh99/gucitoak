d03d5d6928030cdda6e77ab24a3b89428530a2fa
Miftahus Surur Sutowo <62152669+miftahozuki@users.noreply.github.com>
2025-01-25 14:46:03 +0700

    Update _js_omzetpel.php

--- app/Views/admin/laporan/js/_js_omzetpel.php
+++ app/Views/admin/laporan/js/_js_omzetpel.php
@@ -1,49 +1,47 @@
 <script>
-    var options = {
-        chart: {
-            type: 'line',
-            height: 400
-        },
-        series: [{
-            name: 'sales',
-            data: []
-        }],
-        xaxis: {
-            categories: []
-        }
-    };
+    $(document).ready(function() {
+        const chart = new ApexCharts(document.querySelector("#chart"), {
+            chart: {
+                type: 'line',
+                height: 400
+            },
+            series: [{
+                name: 'sales',
+                data: []
+            }],
+            xaxis: {
+                categories: []
+            }
+        });
+        chart.render();
 
-    var chart = new ApexCharts(document.querySelector("#chart"), options);
-    
-    // Render grafik
-    chart.render();
-    
-    $("#lihat").on("click",function(){
-        const idPelanggan = $("#pelanggan").val();
-        console.log(idPelanggan);
-        $.get("<?=BASE_URL?>laporan/get_omzetpel/" + idPelanggan, function(data, status) {
-            console.log(data);
-            if (data) {
-
-                const mdata = JSON.parse(data);
-                // Format data sesuai kebutuhan ApexCharts
-                const bulan = Object.keys(mdata);  // Data kategori (bulan-tahun)
-                const penjualan = Object.values(mdata).map(Number);
-
-                // Update data grafik
+        function updateChart(idp) {
+            $.get(`<?= BASE_URL ?>laporan/get_omzetpel/${idp}`, function(data) {
+                const mdata = data ? JSON.parse(data) : {};
                 chart.updateSeries([{
                     name: 'sales',
-                    data: penjualan
+                    data: Object.values(mdata).map(Number)
                 }]);
-
                 chart.updateOptions({
                     xaxis: {
-                        categories: bulan
+                        categories: Object.keys(mdata)
                     }
                 });
-            } else {
+            }).fail(() => {
                 console.error("Gagal mengambil data dari server");
-            }
-        })
+                chart.updateSeries([{
+                    name: 'sales',
+                    data: []
+                }]);
+                chart.updateOptions({
+                    xaxis: {
+                        categories: []
+                    }
+                });
+            });
+        }
+
+        updateChart($("#pelanggan").val());
+        $("#lihat").on("click", () => updateChart($("#pelanggan").val()));
     });
 </script>
