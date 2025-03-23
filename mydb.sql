a86d8d783da7b8375e43ffdfc6a7d03f6245f45b
Miftahus Surur Sutowo <62152669+miftahozuki@users.noreply.github.com>
2025-03-23 17:01:34 +0700

    Update mydb.sql

--- mydb.sql
+++ mydb.sql
@@ -120,7 +120,7 @@ LOCK TABLES `barang_detail` WRITE;
 /*!40000 ALTER TABLE `barang_detail` DISABLE KEYS */;
 INSERT INTO `barang_detail` VALUES
 ('139875032890250525',10,'2025-05-25','2024-11-02 16:28:46'),
-('139875032890250625',8,'2025-06-25','2024-11-02 16:28:46');
+('139875032890250625',10,'2025-06-25','2024-08-02 16:28:46');
 /*!40000 ALTER TABLE `barang_detail` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -447,7 +447,7 @@ CREATE TABLE `pembayaran` (
   PRIMARY KEY (`id`),
   KEY `pembayaran_ibfk_1` (`nonota`),
   CONSTRAINT `pembayaran_ibfk_1` FOREIGN KEY (`nonota`) REFERENCES `penjualan` (`nonota`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -457,11 +457,13 @@ CREATE TABLE `pembayaran` (
 LOCK TABLES `pembayaran` WRITE;
 /*!40000 ALTER TABLE `pembayaran` DISABLE KEYS */;
 INSERT INTO `pembayaran` VALUES
-(3,'000001'),
+(6,'000001'),
 (1,'000002'),
 (2,'000002'),
 (4,'000002'),
-(5,'000002');
+(5,'000002'),
+(7,'000003'),
+(17,'000003');
 /*!40000 ALTER TABLE `pembayaran` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -491,7 +493,10 @@ LOCK TABLES `pembayaran_detail` WRITE;
 INSERT INTO `pembayaran_detail` VALUES
 (1,'2024-12-05',10000,'y'),
 (4,'2025-01-14',19,''),
-(5,'2025-01-14',12,'maknyus');
+(5,'2025-01-14',12,'maknyus'),
+(6,'2025-03-21',100,''),
+(7,'2025-03-23',40000,'cicil #1'),
+(17,'2025-03-23',1000,'');
 /*!40000 ALTER TABLE `pembayaran_detail` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -630,9 +635,10 @@ LOCK TABLES `penjualan` WRITE;
 /*!40000 ALTER TABLE `penjualan` DISABLE KEYS */;
 INSERT INTO `penjualan` VALUES
 ('000001','2024-12-03 00:00:00',1,1,'tempo',7,0,0.00,1),
-('000002','2024-11-13 02:25:36',2,4,'tempo',5,0,0.00,0),
+('000002','2024-11-13 02:25:36',2,4,'tempo',5,40000,0.10,0),
 ('000003','2024-11-13 02:25:36',2,4,'tempo',5,0,0.00,1),
-('000004','2025-01-21 07:54:27',1,3,'cash',0,0,0.00,0);
+('000004','2025-03-20 06:33:31',1,2,'cash',0,1000,0.11,0),
+('000005','2025-03-20 07:32:14',1,2,'cash',0,50000,0.10,0);
 /*!40000 ALTER TABLE `penjualan` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -663,8 +669,7 @@ LOCK TABLES `penjualan_detail` WRITE;
 INSERT INTO `penjualan_detail` VALUES
 ('000001','139875032890250525',2),
 ('000002','139875032890250525',8),
-('000003','139875032890250625',8),
-('000004','139875032890250625',1);
+('000003','139875032890250625',8);
 /*!40000 ALTER TABLE `penjualan_detail` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -773,10 +778,14 @@ CREATE TABLE `retur_jual` (
   `tanggal` datetime NOT NULL,
   `pelanggan_id` int(11) NOT NULL,
   `alasan` varchar(100) NOT NULL,
+  `nonota` varchar(6) NOT NULL,
+  `is_used` tinyint(1) NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`),
   KEY `retur_jual_ibfk_1` (`pelanggan_id`),
-  CONSTRAINT `retur_jual_ibfk_1` FOREIGN KEY (`pelanggan_id`) REFERENCES `pelanggan` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+  KEY `retur_jual_ibfk_2` (`nonota`),
+  CONSTRAINT `retur_jual_ibfk_1` FOREIGN KEY (`pelanggan_id`) REFERENCES `pelanggan` (`id`),
+  CONSTRAINT `retur_jual_ibfk_2` FOREIGN KEY (`nonota`) REFERENCES `penjualan` (`nonota`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -786,7 +795,8 @@ CREATE TABLE `retur_jual` (
 LOCK TABLES `retur_jual` WRITE;
 /*!40000 ALTER TABLE `retur_jual` DISABLE KEYS */;
 INSERT INTO `retur_jual` VALUES
-(1,'2024-12-28 11:13:31',1,'');
+(2,'2025-03-23 06:50:24',2,'','000002',0),
+(3,'2025-03-23 09:56:05',1,'','000001',0);
 /*!40000 ALTER TABLE `retur_jual` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -815,7 +825,8 @@ CREATE TABLE `retur_jual_detail` (
 LOCK TABLES `retur_jual_detail` WRITE;
 /*!40000 ALTER TABLE `retur_jual_detail` DISABLE KEYS */;
 INSERT INTO `retur_jual_detail` VALUES
-(1,'139875032890250525',3);
+(2,'139875032890250525',1),
+(3,'139875032890250525',1);
 /*!40000 ALTER TABLE `retur_jual_detail` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -957,7 +968,7 @@ CREATE TABLE `user_role` (
   UNIQUE KEY `pengguna_id_2` (`pengguna_id`),
   KEY `pengguna_id` (`pengguna_id`),
   CONSTRAINT `user_role_ibfk_1` FOREIGN KEY (`pengguna_id`) REFERENCES `pengguna` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=34 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -967,7 +978,7 @@ CREATE TABLE `user_role` (
 LOCK TABLES `user_role` WRITE;
 /*!40000 ALTER TABLE `user_role` DISABLE KEYS */;
 INSERT INTO `user_role` VALUES
-(22,9,'{\"setup\":[\"daftar_pengguna\",\"daftar_sales\"],\"laporan\":[\"mutasi_stok\",\"outlet_idle\"]}'),
+(22,9,'{\"setup\":[\"daftar_pengguna\",\"daftar_sales\"],\"laporan\":[\"barang_expired\",\"mutasi_stok\",\"outlet_idle\"]}'),
 (30,10,'[]');
 /*!40000 ALTER TABLE `user_role` ENABLE KEYS */;
 UNLOCK TABLES;
@@ -981,4 +992,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*M!100616 SET NOTE_VERBOSITY=@OLD_NOTE_VERBOSITY */;
 
--- Dump completed on 2025-03-16 14:29:53
+-- Dump completed on 2025-03-23 17:01:28
